4d6ed32999e7143df4a07124d1831d0730580060
Marinarty <tsiganosha@yandex.ru>
2019-09-25 13:46:23 +0200

    сделана генерация объявлений и отрисовка пинов на карте

--- js/main.js
+++ js/main.js
@@ -10,8 +10,6 @@ var photos = [
   'http://o0.github.io/assets/images/tokyo/hotel2.jpg',
   'http://o0.github.io/assets/images/tokyo/hotel3.jpg'
 ]; // адреса фотографий
-var x;
-var y;
 
 // функция рандома
 function getRandomInt(min, max) {
@@ -21,17 +19,20 @@ function getRandomInt(min, max) {
 }
 
 // функция генерации массива из объектов-объявлений
-function createAds() {
+function createAds(adsLimit) {
 
-  for (var i = 0; i < adsNumber; i++) {
+  for (var i = 0; i < adsLimit; i++) {
+    var x = getRandomInt(100, 900); // координаты метки на карте
+    var y = getRandomInt(200, 600);
+
     adverts[i] = {
       'author': {
         'avatar': 'img/avatars/user0' + (i + 1) + '.png' // аватар автора объявления
       },
 
       'location': {
-        x: getRandomInt(130, 630), // случайное число, координата x метки на карте. Значение ограничено размерами блока, в котором перетаскивается метка
-        y: getRandomInt(130, 630), // посчитать координаты , беря во внимание ширину и  высоту метки
+        x: x,
+        y: y
       },
 
       'offer': {
@@ -44,7 +45,7 @@ function createAds() {
         'checkin': checks[getRandomInt(0, checks.length - 1)],
         'checkout': checks[getRandomInt(0, checks.length - 1)],
         'features': features.slice(Math.floor(Math.random() * features.length)), // массив строк случайной длины из ниже предложенных: "wifi", "dishwasher", "parking", "washer", "elevator", "conditioner"
-        'description': 'Прекрасное описания объявления ' + (i + 1),
+        'description': 'Прекрасное описания объявления ' + (i + 1), // описание объявления
         'photos': photos.slice(Math.floor(Math.random() * photos.length)) // массив строк случайной длины, содержащий адреса фотографий
       },
     };
@@ -52,7 +53,7 @@ function createAds() {
   return adverts;
 }
 
-createAds();
+createAds(adsNumber);
 
 var map = document.querySelector('.map');
 map.classList.remove('map--faded');
@@ -60,13 +61,13 @@ var pinTemplate = document.querySelector('#pin').content.querySelector('.map__pi
 var mapPins = document.querySelector('.map__pins');
 var fragment = document.createDocumentFragment();
 
-var createPins = function (adverts) {
-  var advertsElement = pinTemplate.cloneNode(true);
-  advertsElement.style.left = adverts.location.x + 'px';
-  advertsElement.style.top = adverts.location.y + 'px';
-  advertsElement.querySelector('img').src = adverts.author.avatar;
-  advertsElement.querySelector('img').alt = adverts.offer.title;
-  return advertsElement;
+var createPins = function (ads) {
+  var adsElement = pinTemplate.cloneNode(true);
+  adsElement.style.left = ads.location.x + 'px';
+  adsElement.style.top = ads.location.y + 'px';
+  adsElement.querySelector('img').src = ads.author.avatar;
+  adsElement.querySelector('img').alt = ads.offer.title;
+  return adsElement;
 };
 
 for (var i = 0; i < adverts.length; i++) {
