fb6063322d614e05e07ccb9c7e608c2642868107
Marinarty <tsiganosha@yandex.ru>
2019-09-26 15:42:23 +0200

    начала делать задания Больше деталей

--- js/main.js
+++ js/main.js
@@ -75,3 +75,37 @@ for (var i = 0; i < adverts.length; i++) {
 }
 
 mapPins.appendChild(fragment);
+
+// На основе первого по порядку элемента из сгенерированного массива и шаблона #card создайте DOM-элемент объявления, заполните его данными из объекта:
+
+//     Выведите заголовок объявления offer.title в заголовок .popup__title.
+//     Выведите адрес offer.address в блок .popup__text--address.
+//     Выведите цену offer.price в блок .popup__text--price строкой вида {{offer.price}}₽/ночь. Например, 5200₽/ночь.
+//     В блок .popup__type выведите тип жилья offer.type: Квартира для flat, Бунгало для bungalo, Дом для house, Дворец для palace.
+//     Выведите количество гостей и комнат offer.rooms и offer.guests в блок .popup__text--capacity строкой вида {{offer.rooms}} комнаты для {{offer.guests}} гостей. Например, 2 комнаты для 3 гостей.
+//     Время заезда и выезда offer.checkin и offer.checkout в блок .popup__text--time строкой вида Заезд после {{offer.checkin}}, выезд до {{offer.checkout}}. Например, заезд после 14:00, выезд до 12:00.
+//     В список .popup__features выведите все доступные удобства в объявлении.
+//     В блок .popup__description выведите описание объекта недвижимости offer.description.
+//     В блок .popup__photos выведите все фотографии из списка offer.photos. Каждая из строк массива photos должна записываться как src соответствующего изображения.
+//     Замените src у аватарки пользователя — изображения, которое записано в .popup__avatar — на значения поля author.avatar отрисовываемого объекта.
+
+// Вставьте полученный DOM-элемент в блок .map перед блоком.map__filters-container.
+
+var cardTemplate = document.querySelector('#card').content.querySelector('.map__card');
+
+var createPopup = function(popup) {
+  var popupElement = cardTemplate.cloneNode(true);
+  popupElement.querySelector('.popup__title').textContent = popup.offer.title;
+  popupElement.querySelector('.popup__text--address').textContent = popup.offer.address;
+  popupElement.querySelector('.popup__text--price').textContent = popup.offer.price + ' ₽/ночь';
+//     В блок .popup__type выведите тип жилья offer.type: Квартира для flat, Бунгало для bungalo, Дом для house, Дворец для palace.
+  popupElement.querySelector('.popup__text--capacity').textContent = popup.offer.rooms + ' комнаты для ' + popup.offer.guests + ' гостей.';
+  popupElement.querySelector('.popup__text--time').textContent = 'Заезд после ' + popup.offer.checkin + ', выезд до ' + popup.offer.checkout;
+  popupElement.querySelector('.popup__features').textContent = popup.offer.features; // что-то надо делать с фьючерсами, чтобы отображались картинки, а не названия
+  popupElement.querySelector('.popup__description').textContent = popup.offer.description;
+  popupElement.querySelector('.popup__photos').textContent = popup.offer.photos; // что-то надо делать
+  popupElement.querySelector('.popup__avatar').src = popup.author.avatar;
+  return popupElement;
+};
+
+document.querySelector('.map').insertBefore(createPopup(adverts[0]), document.querySelector('.map__filters-container'));
